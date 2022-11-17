3b5350682e34cd2f938c0af6727fb7696aeee0ea
Isaias Medina <isamed92@gmail.com>
2022-11-17 16:54:28 -0300

    feature: create structure, stack navigator and icons

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native'
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Text>Hola mundo</Text>
+    </NavigationContainer>
+  )
+}
+
+export default App
